9f03a18f6150233ba483e45603142ec88e38b893
Mook <m.ookm.oz+git@gmail.com>
2014-02-08 21:07:15 -0800

    rename server script

--- server/server.py
+++ server/server.py
@@ -1,20 +1,39 @@
-#!/usr/bin/env python
-
-print "Starting server...."
-
+import ctypes
 import os
-from wsgiref.simple_server import make_server
-import sys
-from os.path import dirname, join
+import os.path
 
-import_path = join(dirname(__file__), "dxr")
-sys.path.insert(0, import_path)
+# Fake out the path to libtrilite.so
+old_CDLL = ctypes.CDLL
+def replacement_CDLL(name, mode=ctypes.DEFAULT_MODE, handle=None, use_errno=False, use_last_error=False):
+    if name == "libtrilite.so":
+        name = os.path.expanduser("~/dxr/trilite/libtrilite.so")
+    return old_CDLL(name, mode, handle, use_errno, use_last_error)
+ctypes.CDLL = replacement_CDLL
+
 try:
     from dxr.app import make_app
-finally:
-    sys.path.remove(import_path)
 
-def application(environ, start_response):
-    print "Making new app"
-    return make_app(os.environ["DXR_FOLDER"])(environ, start_response)
-make_server("", int(os.environ["VCAP_APP_PORT"]), application).serve_forever()
+    app = make_app(os.path.abspath(os.environ["DXR_FOLDER"]))
+    app.debug = True
+    app.run(host=os.environ["VCAP_APP_HOST"],
+            port=int(os.environ["VCAP_APP_PORT"]),
+            processes=1,
+            threaded=False)
+except Exception as ex:
+    import logging
+    import sys
+    logging.basicConfig(stream=sys.stdout)
+    logging.root.exception(ex)
+
+    # Fall back to avoid begin killed by Stackato
+    from flask import Flask
+    app = Flask("DXR-server-fallback",
+                instance_path=os.path.abspath(os.environ["DXR_FOLDER"]))
+    @app.route("/")
+    def dummy():
+        return "Application failed to start: %s" % (ex,)
+    app.debug = True
+    app.run(host=os.environ["VCAP_APP_HOST"],
+            port=int(os.environ["VCAP_APP_PORT"]),
+            processes=1,
+            threaded=False)
